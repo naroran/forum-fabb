df545e7e816d05dc1d0f0c5d71fd8daff6455a9c
faci <naroran@yahoo.fr>
2020-05-22 23:36:53 +0200

    Update config.php

--- forum/application/config/config.php
+++ forum/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://www.forum-fabb.com/forum/';
+$config['base_url'] = '%base_url%';
 
 /*
 |--------------------------------------------------------------------------
