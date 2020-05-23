a6c9aff47d247773f31faa2b2a596c4252611441
faci <naroran@yahoo.fr>
2020-05-23 16:01:56 +0200

    Add files via upload

---
+++ forum/application/views/error404.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+ <head>
+   <title>404 Page Not Found</title>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+   <style>
+   body{
+     width: 99%;
+     height: 100%;
+     background-color: mediumturquoise;
+     color: white;
+     font-family: sans-serif;
+   }
+   div {
+     position: absolute;
+     width: 400px;
+     height: 300px;
+     z-index: 15;
+     top: 45%;
+     left: 50%;
+     margin: -100px 0 0 -200px;
+     text-align: center;
+   }
+   h1,h2{
+     text-align: center;
+   }
+   h1{
+     font-size: 60px;
+     margin-bottom: 10px;
+     border-bottom: 1px solid white;
+     padding-bottom: 10px;
+   }
+   h2{
+     margin-bottom: 40px;
+   }
+   a{
+     margin-top:10px;
+     text-decoration: none;
+     padding: 10px 25px;
+     background-color: ghostwhite;
+     color: black;
+     margin-top: 20px;
+   }
+   </style>
+ </head>
+ <body>
+   <div>
+     <h1>404</h1>
+     <h2>Page Introuvable</h2>
+     <a class="btn btn-warning btn-lg" href='<?= base_url(); ?>' >Retour Page Accueil</a>
+   </div>
+ </body>
+</html>
