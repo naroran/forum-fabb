9b40b3ce1025fbec12286d2deb571f414258aca3
faci <naroran@yahoo.fr>
2020-05-23 23:19:16 +0200

    Add files via upload

---
+++ forum/system/language/french/form_validation_lang.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ * @author	CodeIgniter community
+ * @copyright	Copyright (c) 2014-2019, British Columbia Institute of Technology (https://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['form_validation_required']              = '<span style="color:red;font-size:0.8em;">Le champ :<b> ({field})</b> est obligatoire..!</span>';
+$lang['form_validation_isset']                 = '<span style="color:red;font-size:0.8em;">Le champ {field} doit avoir une valeur./sp
+';
+$lang['form_validation_valid_email']           = '<span style="color:red;font-size:0.8em;">Le champ {field} doit contenir une adresse email valide.</span>';
+$lang['form_validation_valid_emails']          = '<span style="color:red;font-size:0.8em;">Le champ {field} ne peut contenir que des adresses email valides.</span>';
+$lang['form_validation_valid_url']             = '<span style="color:red;font-size:0.8em;">Le champ {field} doit contenir une URL valide.</span>';
+$lang['form_validation_valid_ip']              = '<span style="color:red;font-size:0.8em;">Le champ {field} doit contenir une IP valide.</span>';
+$lang['form_validation_min_length']            = '<span style="color:red;font-size:0.8em;">Le champ {field} doit contenir au moins {param} caractères.</span>';
+$lang['form_validation_max_length']            = '<span style="color:red;font-size:0.8em;">Le champ {field} ne peut contenir plus de {param} caractères.</span>';
+$lang['form_validation_exact_length']          = '<span style="color:red;font-size:0.8em;">Le champ {field} doit contenir exactement {param} caractères.</span>';
+$lang['form_validation_alpha']                 = '<span style="color:red;font-size:0.8em;">Le champ {field} ne peut contenir que des caractères alphabétiques.</span>';
+$lang['form_validation_alpha_numeric']         = '<span style="color:red;font-size:0.8em;">Le champ {field} ne peut contenir que des caractères alphanumériques.</span>';
+$lang['form_validation_alpha_numeric_spaces']  = '<span style="color:red;font-size:0.8em;">Le champ {field} ne peut contenir que des caractères alphanumériques et des espaces.</span>';
+$lang['form_validation_alpha_dash']            = '<span style="color:red;font-size:0.8em;">Le champ {field} ne peut contenir que des caractères alphanumériques, des caractères de soulignement et des traits d\'union.</span>';
+$lang['form_validation_numeric']               = '<span style="color:red;font-size:0.8em;">Le champ {field} doit contenir un nombre (caractères numériques).</span>';
+$lang['form_validation_is_numeric']            = '<span style="color:red;font-size:0.8em;">Le champ {field} ne peut contenir que de signes du type nombre.</span>';
+$lang['form_validation_integer']               = '<span style="color:red;font-size:0.8em;">Le champ {field} doit contenir un nombre entier.</span>';
+$lang['form_validation_regex_match']           = '<span style="color:red;font-size:0.8em;">Le champ {field} n\'utilise pas le bon format.</span>';
+$lang['form_validation_matches']               = '<span style="color:red;font-size:0.8em;">Le champ {field} doit correspondre au champ {param}.</span>';
+$lang['form_validation_differs']               = '<span style="color:red;font-size:0.8em;">Le champ {field} doit être différent du champ {param}.</span>';
+$lang['form_validation_is_unique']             = '<span style="font-size:0.8em; color:red">La valeur du champ {field} est déjà utilisée ou est un mot réservé.</span>';
+$lang['form_validation_is_natural']            = '<span style="color:red;font-size:0.8em;">Le champ {field} ne peut contenir que des nombres positifs.</span>';
+$lang['form_validation_is_natural_no_zero']    = '<span style="color:red;font-size:0.8em;">Le champ {field} ne peut contenir que des nombres plus grands que zéro.</span>';
+$lang['form_validation_decimal']               = '<span style="color:red;font-size:0.8em;">Le champ {field} doit contenir un nombre décimal.</span>';
+$lang['form_validation_less_than']             = '<span style="color:red;font-size:0.8em;">Le champ {field} doit contenir un nombre inférieur à {param}.</span>';
+$lang['form_validation_less_than_equal_to']    = '<span style="color:red;font-size:0.8em;">Le champ {field} doit contenir un nombre inférieur ou égal à {param}.</span>';
+$lang['form_validation_greater_than']          = '<span style="color:red;font-size:0.8em;">Le champ {field} doit contenir un nombre supérieur à {param}.</span>';
+$lang['form_validation_greater_than_equal_to'] = '<span style="color:red;font-size:0.8em;">Le champ {field} doit contenir un nombre supérieur ou égal à {param}.</span>';
+$lang['form_validation_error_message_not_set'] = '<span style="color:red;font-size:0.8em;">Impossible d\'accéder à un message d\'erreur correspondant à votre champ nommé {field}.</span>';
+$lang['form_validation_in_list']               = '<span style="color:red;font-size:0.8em;">Le champ {field} doit avoir une de ces valeurs : {param}.</span>';
